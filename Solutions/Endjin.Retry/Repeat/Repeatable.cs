cc2231c9e07e6195b8c63623a0962100a7f903ff
mwadams <matthew.adams@endjin.com>
2013-10-16 13:42:59 +0100

    Added Repeatable to perform periodic tasks

---
+++ Solutions/Endjin.Retry/Repeat/Repeatable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Endjin.Core.Repeat
+{
+    public static class Repeatable
+    {
+        public static void Repeat(CancellationToken cancellationToken, TimeSpan periodicity, Action<CancellationToken> action)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                action(cancellationToken);
+                cancellationToken.WaitHandle.WaitOne(periodicity);
+            }
+        }
+
+        public static async Task RepeatAsync(CancellationToken cancellationToken, TimeSpan periodicity, Func<CancellationToken, Task> action)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await action(cancellationToken);
+                cancellationToken.WaitHandle.WaitOne(periodicity);
+            }
+        }
+    }
+}
