8bc63e0150d38b17cb034ec64e2403f6647f1d84
mwadams <matthew.adams@endjin.com>
2013-10-17 15:56:44 +0100

    Added periodicity strategy

--- Solutions/Endjin.Retry/Repeat/Repeatable.cs
+++ Solutions/Endjin.Retry/Repeat/Repeatable.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Endjin.Core.Repeat.Strategies;
 
 namespace Endjin.Core.Repeat
 {
@@ -11,19 +12,29 @@ namespace Endjin.Core.Repeat
     {
         public static void Repeat(CancellationToken cancellationToken, TimeSpan periodicity, Action<CancellationToken> action)
         {
+            Repeat(cancellationToken, new LinearPeriodicityStrategy(periodicity), action);
+        }
+
+        public static Task RepeatAsync(CancellationToken cancellationToken, TimeSpan periodicity, Func<CancellationToken, Task> action)
+        {
+            return RepeatAsync(cancellationToken, new LinearPeriodicityStrategy(periodicity), action);
+        }
+
+        public static void Repeat(CancellationToken cancellationToken, IPeriodicityStrategy periodicity, Action<CancellationToken> action)
+        {
             while (!cancellationToken.IsCancellationRequested)
             {
                 action(cancellationToken);
-                cancellationToken.WaitHandle.WaitOne(periodicity);
+                cancellationToken.WaitHandle.WaitOne(periodicity.GetPeriodicity());
             }
         }
 
-        public static async Task RepeatAsync(CancellationToken cancellationToken, TimeSpan periodicity, Func<CancellationToken, Task> action)
+        public static async Task RepeatAsync(CancellationToken cancellationToken, IPeriodicityStrategy periodicity, Func<CancellationToken, Task> action)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
                 await action(cancellationToken);
-                cancellationToken.WaitHandle.WaitOne(periodicity);
+                cancellationToken.WaitHandle.WaitOne(periodicity.GetPeriodicity());
             }
         }
     }
