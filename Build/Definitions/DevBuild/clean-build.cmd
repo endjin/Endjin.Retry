cc26b35553b7e0bc6387fbd968379f42941d0933
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2013-10-24 21:17:47 +0100

    Update build invocation scripts to use MSBuild v12 (VS2013)

--- Build/Definitions/DevBuild/clean-build.cmd
+++ Build/Definitions/DevBuild/clean-build.cmd
@@ -3,7 +3,7 @@ setlocal
 REM Ensure we know whether we are running on a 32it or 64bit system
 REM IF "%PROCESSOR_ARCHITECTURE%" == "x86" SET FRAMEWORKDIR=Framework
 REM IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" SET FRAMEWORKDIR=Framework64
-SET MSBUILDEXE=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+SET MSBUILDEXE="%ProgramFiles(X86)%\MSBuild\12.0\bin\msbuild.exe"
 
 REM set msbuildemitsolution=1
 
