16ac1300e7003f47c1cb4f2254c9b434a84bd7b9
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2013-12-18 10:34:38 +0000

    Add new overloads that set an intial delay before repeating.

--- Solutions/Endjin.Retry/Repeat/Repeatable.cs
+++ Solutions/Endjin.Retry/Repeat/Repeatable.cs
@@ -13,12 +13,22 @@
     {
         public static void Repeat(CancellationToken cancellationToken, TimeSpan periodicity, Action<CancellationToken> action)
         {
-            Repeat(cancellationToken, new LinearPeriodicityStrategy(periodicity), action);
+            Repeat(cancellationToken, new LinearPeriodicityStrategy(periodicity), TimeSpan.FromSeconds(0), action);
+        }
+
+        public static void Repeat(CancellationToken cancellationToken, TimeSpan periodicity, TimeSpan initialDelay, Action<CancellationToken> action)
+        {
+            Repeat(cancellationToken, new LinearPeriodicityStrategy(periodicity), initialDelay, action);
         }
 
         public static Task RepeatAsync(CancellationToken cancellationToken, TimeSpan periodicity, Func<CancellationToken, Task> action)
         {
-            return RepeatAsync(cancellationToken, new LinearPeriodicityStrategy(periodicity), action);
+            return RepeatAsync(cancellationToken, new LinearPeriodicityStrategy(periodicity), TimeSpan.FromSeconds(0), action);
+        }
+
+        public static Task RepeatAsync(CancellationToken cancellationToken, TimeSpan periodicity, TimeSpan initialDelay, Func<CancellationToken, Task> action)
+        {
+            return RepeatAsync(cancellationToken, new LinearPeriodicityStrategy(periodicity), initialDelay, action);
         }
 
         public static void Repeat(CancellationToken cancellationToken, IPeriodicityStrategy periodicity, Action<CancellationToken> action)
@@ -37,6 +47,20 @@
                 await action(cancellationToken);
                 cancellationToken.WaitHandle.WaitOne(periodicity.GetPeriodicity());
             }
+        }
+
+        private static void Repeat(CancellationToken cancellationToken, IPeriodicityStrategy periodicity, TimeSpan initialDelay, Action<CancellationToken> action)
+        {
+            cancellationToken.WaitHandle.WaitOne(initialDelay);
+
+            Repeat(cancellationToken, periodicity, action);
+        }
+
+        private static Task RepeatAsync(CancellationToken cancellationToken, IPeriodicityStrategy periodicity, TimeSpan initialDelay, Func<CancellationToken, Task> action)
+        {
+            cancellationToken.WaitHandle.WaitOne(initialDelay);
+
+            return RepeatAsync(cancellationToken, periodicity, action);
         }
     }
 }
