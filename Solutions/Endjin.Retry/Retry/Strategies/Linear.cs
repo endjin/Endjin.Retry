19322213b1857364ce517d6525cfc654065adbad
Howard.vanRooijen <Howard.vanRooijen@endjin.com>
2013-12-18 13:47:32 +0000

    Added Linear retry strategy

---
+++ Solutions/Endjin.Retry/Retry/Strategies/Linear.cs
@@ -0,0 +1,34 @@
+﻿namespace Endjin.Core.Retry.Strategies
+{
+    using System;
+
+    public class Linear : RetryStrategy
+    {
+        private readonly TimeSpan periodicity;
+        private readonly int maxTries;
+        private int tryCount; 
+
+        public Linear(TimeSpan periodicity, int maxTries)
+        {
+            this.periodicity = periodicity;
+            this.maxTries = maxTries;
+        }
+
+        public override bool CanRetry
+        {
+            get
+            {
+                return this.tryCount < this.maxTries;
+            }
+        }
+
+        public override TimeSpan PrepareToRetry(Exception lastException)
+        {
+            this.AddException(lastException);
+
+            this.tryCount += 1;
+
+            return this.periodicity;
+        }
+    }
+}
