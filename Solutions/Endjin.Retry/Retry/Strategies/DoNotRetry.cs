9394f6edea8e7063eb2693496933b7911e2d429d
Howard.vanRooijen <Howard.vanRooijen@endjin.com>
2013-12-19 10:07:46 +0000

    Add DoNoRetry Strategy.

---
+++ Solutions/Endjin.Retry/Retry/Strategies/DoNotRetry.cs
@@ -0,0 +1,17 @@
+﻿namespace Endjin.Core.Retry.Strategies
+{
+    using System;
+
+    public class DoNotRetry : RetryStrategy
+    {
+        public override bool CanRetry
+        {
+            get { return false; }
+        }
+
+        public override TimeSpan PrepareToRetry(Exception lastException)
+        {
+            return TimeSpan.Zero;
+        }
+    }
+}
