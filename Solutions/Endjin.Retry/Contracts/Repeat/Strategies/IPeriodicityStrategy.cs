d6fb9c39db80af17de6a25dffdcce23d624bb4ec
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2014-01-13 19:03:22 +0000

    Add strong name.

---
+++ Solutions/Endjin.Retry/Contracts/Repeat/Strategies/IPeriodicityStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Endjin.Core.Repeat.Strategies
+{
+    public interface IPeriodicityStrategy
+    {
+        TimeSpan GetPeriodicity();
+
+        void Reset();
+
+        void EnableOneTimeRunImmediate();
+    }
+}
